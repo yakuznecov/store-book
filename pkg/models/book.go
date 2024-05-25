00bb8772f876a20cd0a8cd8c5755bfb66468bf94
archi-innovacii <archi.rosinnovacii@gmail.com>
2024-05-25 17:44:18 +0300

    add models

---
+++ pkg/models/book.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/yakuznecov/store-book/pkg/config"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name string `gorm:""json:"name"`
+	Author string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+// функция инициализации
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
